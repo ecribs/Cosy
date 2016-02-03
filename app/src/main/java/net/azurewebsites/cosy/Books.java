cbff768e59ee352ce4c90dacf730d26c8ebb8813
ecribs <c12710421@mydit.ie>
2016-02-03 16:38:28 +0000

    Open sample pdf
    When an Item is clicked a sample pdf opens in the browser

--- app/src/main/java/net/azurewebsites/cosy/Books.java
+++ app/src/main/java/net/azurewebsites/cosy/Books.java
@@ -1,14 +1,15 @@
 package net.azurewebsites.cosy;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-public class Books extends ActionBarActivity
+public class Books extends Activity
 {
 
     @Override
@@ -30,8 +31,12 @@ public class Books extends ActionBarActivity
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id)
                     {
-                        String books = String.valueOf(parent.getItemAtPosition(position));
-                        Toast.makeText(Books.this, books, Toast.LENGTH_LONG).show();
+                        String url = "https://bitcoin.org/bitcoin.pdf";
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(url));
+                        startActivity(i);
+                        //String books = String.valueOf(parent.getItemAtPosition(position));
+                       // Toast.makeText(Books.this, books, Toast.LENGTH_LONG).show();
                     }
                 }
 
