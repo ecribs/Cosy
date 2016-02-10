cab5b03dc36e52b68c7e02e734036843d487190a
ecribs <c12710421@mydit.ie>
2016-02-10 18:45:40 +0000

    PHP book request and testing files
    adding SQL database and requests for FTP

---
+++ PHP/bookrequest.php
@@ -0,0 +1,88 @@
+<?php
+$serverName = "cosy.database.windows.net";
+$connectionInfo = array( "Database"=>"cosy", "UID"=>"eocribin", "PWD"=>"Ecribin1");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+//echo "we have entered the file <br />"; 
+if( $conn ) 
+{
+	//echo "connection works <br />";
+	
+	
+	
+	$username = $_POST["username"];
+	
+	//echo $username;
+
+	$query = sprintf("SELECT ClassID from Users where Username = ?");
+	$params1 = array( $username);
+    $stmt = sqlsrv_query($conn, $query, $params1);
+	
+	
+	
+	    if ($stmt === false) 
+	{
+		//echo "statement fucked";
+        die(print_r(sqlsrv_errors(), true));
+    }
+		
+		while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+		{
+				$ClassID = ($row['ClassID']);
+				
+		}
+		
+		if( $ClassID==null)
+		{
+			
+			//echo "<br /> no class";
+		}
+		else
+		{
+			//echo $ClassID;
+		}
+		
+	//echo "<br /> statement 2";	
+		
+
+
+			$query = sprintf("select Bookname, SubjectName from books join Subjects on Subjects.SubjectID = Books.subjectID where classID = ?");
+			$params1 = array( $ClassID);
+			$stmt = sqlsrv_query($conn, $query, $params1);
+			print_r($row);
+			
+	if ($stmt === false) 
+	{
+		echo "statement fucked";
+        die(print_r(sqlsrv_errors(), true));
+    }
+				$Bookname = array();
+				$subjectname = array();
+			   $index = 0;
+			   $data = [];
+			   while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+			  {
+				//print_r($row);
+				$Bookname[index] = $row['Bookname'];
+				$data['Bookname'][$index] = $row['Bookname'];
+				$data['SubjectName'][$index] = $row['SubjectName'];
+				//echo "in the loop";
+
+				$index++;
+
+       }
+	   
+			echo json_encode($data);
+		
+		}
+	
+	
+else
+	{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+
+	}
+	
+	
+	
+	?>
